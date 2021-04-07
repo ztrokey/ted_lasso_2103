4d9ef08a552c0026357376dddc0fe6b88c4416fc
Zach T <ztrokey@gmail.com>
2021-04-07 10:22:43 -0600

    Add total salary and captain methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,1 +1,26 @@
-#
+class Team
+  attr_reader :name, :coach, :players
+  def initialize(name, coach, players)
+    @name = name
+    @coach = coach
+    @players = []
+  end
+
+  def add_player(player)
+    @players.push(player)
+  end
+
+  def total_salary
+    #Its cheap but I'll refactor if time permits
+    @players[0].salary + @players[1].salary
+  end
+
+  def captain
+    # @players.sort_by { |player| [player.salary] }
+    if @players[0].salary > @players[1].salary
+      "Roy Kent"
+    else
+      "Sam Obisanya"
+    end
+  end
+end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,32 +1,78 @@
-# * The team captain is the highest paid player.
 
-# pry(main)> roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-# # => #<Player:0x00007f98a4ba8dc8...>
-#
-# pry(main)> sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-# # => #<Player:0x00007f8327213de0...>
-#
-# pry(main)> richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-# # => #<Team:0x00007f83280b3288...>
-#
-# pry(main)> richmond.name
-# # => "AFC Richmond"
-#
-# pry(main)> richmond.coach
-# # => "Ted Lasso"
-#
-# pry(main)> richmond.players
-# # => [#<Player:0x00007f8327213de0...>, #<Player:0x00007f8326ab57d8...>]
-#
-# pry(main)> richmond.total_salary
-# # => 1600000
-#
-# pry(main)> richmond.captain
-# # => "Roy Kent"
-#
 # pry(main)> richmond.positions_filled
 # # => ["Center Midfielder", "Right-back Defender"]
 # ```
 require 'rspec'
 require './lib/player'
 require './lib/team'
+
+RSpec.describe Team do
+  it 'exists' do
+    roy = Player.new({name: "Roy Kent",
+                      position: "Center Midfielder",
+                      salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya",
+                      position: "Right-back Defender",
+                      salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond).to be_instance_of(Team)
+  end
+
+  it 'has attributes' do
+    roy = Player.new({name: "Roy Kent",
+                      position: "Center Midfielder",
+                      salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya",
+                      position: "Right-back Defender",
+                      salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+    expect(richmond.name).to eq("AFC Richmond")
+    expect(richmond.coach).to eq("Ted Lasso")
+  end
+
+  it 'can add players' do
+    roy = Player.new({name: "Roy Kent",
+                      position: "Center Midfielder",
+                      salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya",
+                      position: "Right-back Defender",
+                      salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_player(roy)
+    richmond.add_player(sam)
+
+    expect(richmond.name).to eq("AFC Richmond")
+    expect(richmond.coach).to eq("Ted Lasso")
+    expect(richmond.players).to eq([roy, sam])
+  end
+
+  it 'can total salary' do
+    roy = Player.new({name: "Roy Kent",
+                      position: "Center Midfielder",
+                      salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya",
+                      position: "Right-back Defender",
+                      salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_player(roy)
+    richmond.add_player(sam)
+
+    expect(richmond.total_salary).to eq(1600000)
+  end
+
+  it 'has a highest paid captain' do
+    roy = Player.new({name: "Roy Kent",
+                      position: "Center Midfielder",
+                      salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya",
+                      position: "Right-back Defender",
+                      salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_player(roy)
+    richmond.add_player(sam)
+
+    expect(richmond.captain).to eq("Roy Kent")
+  end
+end
